41c38acb40e599ddf9ab584a416067dfdeefebcf
EndaMolloy <enda.molloy.1@gmail.com>
2017-12-15 10:19:23 +0000

    test of new heatmap input

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,11 +1,28 @@
-var now = moment().endOf('day').toDate();
-var yearAgo = moment().startOf('day').subtract(1, 'year').toDate();
-var chartData = d3.time.days(yearAgo, now).map(function (dateElement) {
-  return {
-    date: dateElement,
-    count: (dateElement.getDay() !== 0 && dateElement.getDay() !== 6) ? Math.floor(Math.random() * 60) : Math.floor(Math.random() * 10)
-  };
-});
+// var now = moment().endOf('day').toDate();
+// var yearAgo = moment().startOf('day').subtract(1, 'year').toDate();
+// var chartData = d3.time.days(yearAgo, now).map(function (dateElement) {
+//   return {
+//     date: dateElement,
+//     count: (dateElement.getDay() !== 0 && dateElement.getDay() !== 6) ? Math.floor(Math.random() * 60) : Math.floor(Math.random() * 10)
+//   };
+// });
+
+var chartData = [
+  {
+    date: new Date(),
+    count: 3
+  },
+  {
+    date: new Date(),
+    count: 4
+  },
+  {
+    date: new Date(),
+    count: 5
+  }
+]
+
+
 var heatmap = calendarHeatmap()
                 .data(chartData)
                 .selector('#cal-heatmap')
