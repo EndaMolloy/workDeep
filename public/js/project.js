d6f8f032438d0910757534534a3a9b2250d0733c
EndaMolloy <enda.molloy.1@gmail.com>
2017-12-22 19:22:42 +0000

    project list update with animation on hover - need to test edge cases

--- public/js/project.js
+++ public/js/project.js
@@ -1,10 +1,11 @@
 let projectList = [];
 let completedList = [];
 let inFocusProject = 0;
+let project = "";
 
 $("ul").on('click','.delete', function(event){
   $(this).parent().fadeOut(500, function(){
-    let project = $(this).text();
+    let project = $(this).text().trim();
     $(this).remove();
     deleteThisproject(project);
 
@@ -15,7 +16,7 @@ $("ul").on('click','.delete', function(event){
 $("ul").on('click','.complete', function(event){
   $(this).parent().fadeOut(500, function(){
     let project = $(this).text();
-    $(this).remove();
+    //$(this).remove();
     addtoCompletedProjects(project)
 
   });
@@ -36,10 +37,17 @@ $(".projects ul").on('click','li', function(event){
 });
 
 $(".projects input[type='text']").keypress(function(event){
+
   if((event.which === 13)&&($(this).val().length>0)){
-    addtoLiveProjects($(this).val());
+    addtoLiveProjects($(this).val().trim());
     $(this).val("");
+    //console.log(projectList.length);
   }
+
+  if(projectList.length === 5){
+    $(".newProject").hide();
+  }
+
   if(projectList.length === 1){
     document.getElementById('taskInput').textContent = projectList[0];
     inFocusProject = 0;
@@ -48,14 +56,16 @@ $(".projects input[type='text']").keypress(function(event){
 });
 
 function deleteThisproject(project){
+
   let arrIndex = projectList.indexOf(project);
 
-
-  if(arrIndex <= inFocusProject){
+  if(inFocusProject > arrIndex)
     inFocusProject--;
-  }
 
   projectList.splice(arrIndex,1);
+  if(projectList.length < 5)
+    $(".newProject").show();
+
   updateListView();
 }
 
@@ -72,20 +82,20 @@ function addtoLiveProjects(projectName){
 
 function updateListView(){
   $(".projects ul").empty();
-    projectList.forEach((project, index) => {
+  projectList.forEach((project, index) => {
+    if(!inFocusProject){
+      inFocusProject = 0;
+    }
 
-      if(inFocusProject === index){
-        $(".projects ul").append("<li><span id='bullet'><i class='fa fa-circle' aria-hidden='true'></i></span><span class='projectName'>"+project+ "</span><span class='complete'><i class='fa fa-check' aria-hidden='true'></i></span><span class='delete'><i class='fa fa-trash-o' aria-hidden='true'></i></span></li>");
-        document.getElementById('taskInput').textContent = project;
+    if(inFocusProject === index){
+      $(".projects ul").append("<li><span id='bullet'><i class='fa fa-circle' aria-hidden='true'></i></span><span class='projectName'>"+project+ "</span><span class='complete icon-wrapper-list'><i class='fa fa-check custom-icon-list' aria-hidden='true'><span class='fix-editor'>&nbsp;</span></i></span><span class='delete icon-wrapper-list'><i class='fa fa-times custom-icon-list' aria-hidden='true'><span class='fix-editor'>&nbsp;</span></i></span></li>");
+      document.getElementById('taskInput').textContent = project;
 
-      }else{
-        $(".projects ul").append("<li><span id='bullet'><i class='fa fa-circle-thin' aria-hidden='true'></i></span><span class='projectName'>"+project+ "</span><span class='complete'><i class='fa fa-check' aria-hidden='true'></i></span><span class='delete'><i class='fa fa-trash-o' aria-hidden='true'></i></span></li>");
-      }
-      //console.log(inFocusProject);
-
-
-    });
-
+    }else{
+      $(".projects ul").append("<li><span id='bullet'><i class='fa fa-circle-thin' aria-hidden='true'></i></span><span class='projectName'>"+project+ "</span><span class='complete icon-wrapper-list'><i class='fa fa-check custom-icon-list' aria-hidden='true'><span class='fix-editor'>&nbsp;</span></i></span><span class='delete icon-wrapper-list'><i class='fa fa-times custom-icon-list' aria-hidden='true'><span class='fix-editor'>&nbsp;</span></i></span></li>");
+    }
+    //console.log(inFocusProject);
+  });
 }
 
 function checkDuplicate(name){
