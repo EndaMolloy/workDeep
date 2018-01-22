afeddff15c46be8cd21562e05a9cf86a3ff162cf
EndaMolloy <enda.molloy.1@gmail.com>
2018-01-22 17:18:18 +0000

    redirect persitently logged in users to their homescreen

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  if(req.isAuthenticated())
+    res.redirect('/users/'+req.user._id)
+  else
     res.render('landing',{layout: 'landing'});
 });
 
 router.get('/login',(req, res) => {
+  if(req.isAuthenticated())
+    res.redirect('/users/'+req.user._id)
+  else
     res.render('login',{layout: 'auth'});
 });
 
 router.get('/signup',(req, res) => {
+  if(req.isAuthenticated())
+    res.redirect('/users/'+req.user._id)
+  else
     res.render('signup',{layout: 'auth'});
 });
 
 
 router.get('/reset',(req, res) => {
+  if(req.isAuthenticated())
+    res.redirect('/users/'+req.user._id)
+  else
     res.render('reset', {layout: 'auth'});
 })
 
 router.get('/verify',(req,res)=> {
+  if(req.isAuthenticated())
+    res.redirect('/users/'+req.user._id)
+  else
     res.render('verify', {layout: 'auth'})
 })
 
